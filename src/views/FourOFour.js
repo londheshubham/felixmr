730dd2535342bbac720916d658a2b8b333246880
londheshubham <londheshubham68@gmail.com>
2022-01-19 02:33:39 +0530

    404 route added

---
+++ src/views/FourOFour.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+const ContactNew = ({
+    className,
+    topOuterDivider,
+    bottomOuterDivider,
+    topDivider,
+    bottomDivider,
+    hasBgColor,
+    invertColor,
+    ...props
+}) => {
+
+    const outerClasses = classNames(
+        'hero section center-content',
+        topOuterDivider && 'has-top-divider',
+        bottomOuterDivider && 'has-bottom-divider',
+        hasBgColor && 'has-bg-color',
+        invertColor && 'invert-color',
+        className
+    );
+
+    const innerClasses = classNames(
+        'hero-inner section-inner',
+        topDivider && 'has-top-divider',
+        bottomDivider && 'has-bottom-divider'
+    );
+
+    return (
+        <section
+            {...props}
+            className={outerClasses}
+        >
+            <div className="container-sm">
+                <div className={innerClasses}>
+                    <div className="hero-content">
+                        <h3 className="mt-30 mb-16" data-reveal-delay="200" style={{fontSize:'20px'}}>
+                           Nothing Here!
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default ContactNew;
